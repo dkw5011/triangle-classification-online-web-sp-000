a2a2b151ad7bd8ade38f70572d33c674587e6b51
Danielle Bergbower <dkw5011@gmail.com>
2020-03-24 14:20:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
-  # write code here
+ def initialize(one,two,three)
+   @one = one
+   @two = two
+   @three = three
+   @kind
+ end
+ 
+ def kind
+   if @one == @two && @two == @three && @one == @three
+   :equilateral
+   elsif @one == @two && @one != @three && @two != @three || @two == @three && @one != @three && @one != @two || @one == @three && @one != @two && @two != @three
+   :isosceles
+   elsif @one != @two && @one != @three && @two != @three
+   :scalene
+   else @one <= 0 || @two <= 0 || @three <= 0
+      raise TriangleError
+   end
+ end
+ class TriangleError < StandardError
+ end
+ 
 end
