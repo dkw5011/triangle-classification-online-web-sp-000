678d79bb8619a768e3ac75732318058ec36816a3
Danielle Bergbower <dkw5011@gmail.com>
2020-03-24 15:24:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
  def kind
    if @one == @two && @two == @three && @one == @three && @one != 0 && @two != 0 && @three != 0
    :equilateral
-   elsif @one == @two && @one != @three && @two != @three || @two == @three && @one != @three && @one != @two || @one == @three && @one != @two && @two != @three && ((@one + @two) > @three && (@one + @three) > @two && (@two + @three) > @one)
+   elsif @one == @two && @one != @three && @two != @three  || @two == @three && @one != @three && @one != @two || @one == @three && @one != @two && @two != @three && ((@one + @two) > @three && (@one + @three) > @two && (@two + @three) > @one)
    :isosceles
    elsif @one != @two && @one != @three && @two != @three && (@one && @two && @three) > 0 && ((@one + @two) > @three && (@one + @three) > @two && (@two + @three) > @one)
    :scalene
